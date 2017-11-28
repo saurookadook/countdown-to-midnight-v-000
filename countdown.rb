83bd8d1413076633747293aa82cefd0d3f4b9d30
Andy Maskiell <maskiella@gmail.com>
2017-11-28 21:17:31 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,10 @@
 #write your code here
+counter = 10
 
-def countdown
+def countdown(counter)
+  until counter = 0
+    puts "#{counter} SECOND(S)!"
+    counter -= 1
+  end
+  puts "HAPPY NEW YEAR!"
 end
